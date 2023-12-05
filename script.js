540e21817f33d2246819580d26bf2e9d283703d8
Iederees Francis <63100527+iederees-create@users.noreply.github.com>
2023-12-05 21:08:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,68 @@
 // script.js
 
-document.getElementById('dataFile').addEventListener('change', handleFileSelect);
+document.addEventListener('DOMContentLoaded', function() {
+    var dataFileInput = document.getElementById('dataFile');
 
-function handleFileSelect(event) {
-    const file = event.target.files[0];
-
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function (e) {
-            processData(e.target.result);
-        };
-        reader.readAsText(file);
+    if (dataFileInput) {
+        dataFileInput.addEventListener('change', handleFileSelect);
+    } else {
+        console.error("Element with ID 'dataFile' not found.");
     }
-}
 
-function processData(csvData) {
-    const rows = csvData.split('\n');
-    const labels = [];
-    const values = [];
+    function handleFileSelect(event) {
+        const file = event.target.files[0];
 
-    rows.forEach((row, index) => {
-        const [label, value] = row.split(',');
-
-        // Check if the row has the expected format
-        if (label !== undefined && value !== undefined) {
-            labels.push(label.trim());
-            values.push(parseFloat(value.trim()));
-        } else {
-            console.error(`Invalid data in row ${index + 1}: ${row}`);
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                processData(e.target.result);
+            };
+            reader.readAsText(file);
         }
-    });
+    }
 
-    // Update the chart
-    updateChart(labels, values);
-}
+    function processData(csvData) {
+        const rows = csvData.split('\n');
+        const labels = [];
+        const values = [];
 
-function updateChart(labels, values) {
-    var ctx = document.getElementById('salesChart').getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Sales',
-                data: values,
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
+        rows.forEach((row, index) => {
+            const [label, value] = row.split(',');
+
+            // Check if the row has the expected format
+            if (label !== undefined && value !== undefined) {
+                labels.push(label.trim());
+                values.push(parseFloat(value.trim()));
+            } else {
+                console.error(`Invalid data in row ${index + 1}: ${row}`);
+            }
+        });
+
+        // Update the chart
+        updateChart(labels, values);
+    }
+
+    function updateChart(labels, values) {
+        var ctx = document.getElementById('salesChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Sales',
+                    data: values,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
                 }
             }
-        }
-    });
-}
+        });
+    }
+});
